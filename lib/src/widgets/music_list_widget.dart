c84e2e7dd67b3908daf0aec48c0d4e0e1f64e888
Amod Suman <amodsumann@gmail.com>
2021-05-02 08:46:34 +0545

    Extracted methods from the build function

--- lib/src/widgets/music_list_widget.dart
+++ lib/src/widgets/music_list_widget.dart
@@ -13,29 +13,6 @@ class MusicListWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) {
-        launchURL() async {
-          if (await canLaunch(musicList[index].url!)) {
-            await launch(musicList[index].url!);
-          }
-        }
-
-        checkInternet() async {
-          var result = await Connectivity().checkConnectivity();
-          if (result == ConnectivityResult.wifi) {
-            launchURL();
-          } else {
-            Fluttertoast.showToast(
-              msg: "No internet connection!",
-              toastLength: Toast.LENGTH_SHORT,
-              gravity: ToastGravity.BOTTOM,
-              timeInSecForIosWeb: 1,
-              backgroundColor: Colors.grey.shade700,
-              textColor: Colors.white,
-              fontSize: 16,
-            );
-          }
-        }
-
         return Container(
           margin: EdgeInsets.only(top: 10, bottom: 10),
           child: Card(
@@ -43,72 +20,9 @@ class MusicListWidget extends StatelessWidget {
               padding: EdgeInsets.all(10),
               child: Column(
                 children: [
-                  Card(
-                    child: Row(
-                      children: [
-                        Container(
-                          width: 50,
-                          height: 50,
-                          child: ClipRRect(
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(5),
-                                bottomLeft: Radius.circular(5),
-                              ),
-                              child:
-                                  Image.network(musicList[index].thumbnail!)),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 10),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                musicList[index].title!,
-                                style: TextStyle(
-                                  fontSize: 20,
-                                ),
-                              ),
-                              Text(
-                                musicList[index].artist!,
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 10),
-                    child: ClipRRect(
-                        borderRadius: BorderRadius.circular(5),
-                        child: Image.network(musicList[index].image!)),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 10),
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        checkInternet();
-                      },
-                      child: Text(
-                        "Buy Now",
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        padding: EdgeInsets.all(16),
-                        primary: Colors.green,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50),
-                        ),
-                      ),
-                    ),
-                  ),
+                  songHeaderContainer(index),
+                  songImageContainer(index),
+                  buyNowContainer(index),
                 ],
               ),
             ),
@@ -117,5 +31,119 @@ class MusicListWidget extends StatelessWidget {
       },
       itemCount: musicList.length,
     );
+  }
+
+  Card songHeaderContainer(int index) {
+    return Card(
+      child: Row(
+        children: [
+          songThumbnailContainer(index),
+          songTitleContainer(index),
+        ],
+      ),
+    );
+  }
+
+  Container songThumbnailContainer(int index) {
+    return Container(
+      width: 50,
+      height: 50,
+      child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(5),
+            bottomLeft: Radius.circular(5),
+          ),
+          child: Image.network(musicList[index].thumbnail!)),
+    );
+  }
+
+  Padding songTitleContainer(int index) {
+    return Padding(
+      padding: EdgeInsets.only(left: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          songNameContainer(index),
+          songArtistContainer(index),
+        ],
+      ),
+    );
+  }
+
+  Text songNameContainer(int index) {
+    return Text(
+      musicList[index].title!,
+      style: TextStyle(
+        fontSize: 20,
+      ),
+    );
+  }
+
+  Text songArtistContainer(int index) {
+    return Text(
+      musicList[index].artist!,
+      style: TextStyle(
+        fontSize: 14,
+        color: Colors.grey,
+      ),
+    );
+  }
+
+  Container songImageContainer(int index) {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(5),
+          child: Image.network(musicList[index].image!)),
+    );
+  }
+
+  Container buyNowContainer(int index) {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {
+          checkInternet(musicList[index].url!);
+        },
+        child: Text(
+          "Buy Now",
+          style: TextStyle(
+            fontSize: 16,
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.all(16),
+          primary: Colors.green,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50),
+          ),
+        ),
+      ),
+    );
+  }
+
+  launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    }
+  }
+
+  checkInternet(String url) async {
+    var result = await Connectivity().checkConnectivity();
+    if ((result == ConnectivityResult.wifi) ||
+        (result == ConnectivityResult.mobile)) {
+      launchURL(url);
+    } else {
+      Fluttertoast.showToast(
+        msg: "No internet connection!",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.grey.shade700,
+        textColor: Colors.white,
+        fontSize: 16,
+      );
+    }
   }
 }
